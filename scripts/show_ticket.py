44914fc992d969ce02dad8b61ec760b5975d0151
Naoya Horiguchi <nao.horiguchi@gmail.com>
2020-05-23 11:03:43 +0900

    add some python scripts
    Signed-off-by: Naoya Horiguchi <nao.horiguchi@gmail.com>

---
+++ scripts/show_ticket.py
@@ -0,0 +1,112 @@
+import sys
+import json
+import os
+import re
+
+import urllib.request
+import ssl
+import pprint
+pp = pprint.PrettyPrinter(indent=2)
+
+fg = lambda text, color: "\33[38;5;" + str(color) + "m" + text + "\33[0m"
+bg = lambda text, color: "\33[48;5;" + str(color) + "m" + text + "\33[0m"
+
+showJournals = False
+if os.environ.get('SHOW_JOURNALS') and re.match(r'true', os.environ.get('SHOW_JOURNALS'), re.IGNORECASE):
+    showJournals = True
+
+scontext = ssl.SSLContext(ssl.PROTOCOL_TLS)
+url = "%s/issues.json?key=%s&issue_id=605&include=relations&status_id=*" % (os.environ.get('RM_BASEURL'), os.environ.get('RM_KEY'))
+url = "%s/issues/%s.json?key=%s&include=journals" % (os.environ.get('RM_BASEURL'), sys.argv[1], os.environ.get('RM_KEY'))
+header = {}
+
+opener = urllib.request.build_opener()
+opener.addheaders = [('Accept', 'application/json')]
+urllib.request.install_opener(opener)
+
+response = urllib.request.urlopen(url, context=scontext)
+data = response.read()
+text = data.decode('utf-8')
+encoding = response.info().get_content_charset('utf-8')
+text = json.loads(data.decode(encoding))
+
+issue = text['issue']
+
+print("#+DoneRatio: %s" % issue['done_ratio'])
+print("#+Status: %s" % issue['status']['name'])
+print("#+Subject: %s" % issue['subject'])
+print("#+Project: %s" % issue['project']['name'])
+print("#+Tracker: %s" % issue['tracker']['name'])
+if 'category' in issue.keys():
+    print("#+Category: %s" % issue['category']['name'])
+print("#+Priority: %s" % issue['priority']['name'])
+if 'parent' in issue.keys():
+    print("#+ParentIssue: %s" % issue['parent']['id'])
+if 'assigned_to' in issue.keys():
+    print("#+Assigned: %s" % issue['assigned_to']['name'])
+print("#+Estimate: %s" % issue['estimated_hours'])
+print("#+DueDate: %s" % issue['due_date'])
+
+print(issue['description'].replace('\r', ''))
+
+def print_six(row, format):
+    for col in range(6):
+        color = row*6 + col + 4
+        if color>=0:
+            text = "{:3d}".format(color)
+            print (format(text,color), end=" ")
+        else:
+            print("   ", end=" ")
+
+if showJournals != True:
+    exit(0)
+
+import difflib
+for journal in reversed(issue['journals']):
+    print(bg(fg("Journal ID: %s" % journal['id'], 11), 102))
+    print(fg("Author: %s" % journal['user']['name'], 150))
+    print(fg("Date: %s" % journal['created_on'], 213))
+    attrs = []
+    descChange = False
+    for detail in journal['details']:
+        if detail['property'] != 'attr':
+            continue
+        if detail['name'] == 'description':
+            descChange = detail
+        else:
+            attrs.append(detail)
+
+    if attrs or journal['notes']:
+        print("")
+        for attr in attrs:
+            print('    %s: %s -> %s' % (attr['name'], attr['old_value'], attr['new_value']))
+        if journal['notes']:
+            print("")
+            print(re.sub('^', ' '*4, journal['notes'], flags=re.MULTILINE))
+        print("")
+    if descChange:
+        if not descChange['old_value']:
+            descChange['old_value'] = ''
+        if not descChange['new_value']:
+            descChange['new_value'] = ''
+        for line in difflib.unified_diff(descChange['old_value'].replace('\r', '').split('\n'), descChange['new_value'].replace('\r', '').split('\n')):
+            if line == '--- \n' or line == '+++ \n':
+                continue
+            if line[0] == '@':
+                print(fg(line.rstrip(), 3))
+            elif line[0] == '+':
+                print(fg(line, 2))
+            elif line[0] == '-':
+                print(fg(line, 1))
+            else:
+                print(line)
+
+# blocked
+# blocks
+# copied_from
+# copied_to
+# duplicated
+# duplicates
+# follows
+# precedes
+# relates
---
+++ scripts/use_json.py
@@ -0,0 +1,107 @@
+import sys
+import json
+import os
+import re
+
+showClosed = False
+showColor= False
+projects = None
+tickets = None
+grouping = False
+if os.environ.get('SHOWCLOSED') and re.match(r'true', os.environ.get('SHOWCLOSED'), re.IGNORECASE):
+    showClosed = True
+if os.environ.get('COLOR') and re.match(r'true', os.environ.get('COLOR'), re.IGNORECASE):
+    showColor = True
+if os.environ.get('PROJECTS'):
+    projects = list(map(int, os.environ.get('PROJECTS').rsplit(',')))
+if os.environ.get('TICKETS'):
+    tickets = list(map(int, os.environ.get('TICKETS').rsplit(',')))
+if os.environ.get('GROUPING') and re.match(r'true', os.environ.get('GROUPING'), re.IGNORECASE):
+    grouping = True
+
+subjects = {}
+trackers = {}
+status = {}
+ratios = {}
+updateds = {}
+closeds = {}
+pjs = {}
+
+pjIds = {}
+pjNames = {}
+globalIds = []
+
+fg = lambda text, color: "\33[38;5;" + str(color) + "m" + text + "\33[0m"
+bg = lambda text, color: "\33[48;5;" + str(color) + "m" + text + "\33[0m"
+
+def print_six(row, format):
+    for col in range(6):
+        color = row*6 + col + 4
+        if color>=0:
+            text = "{:3d}".format(color)
+            print (format(text,color), end=" ")
+        else:
+            print("   ", end=" ")
+
+with open(sys.argv[1]) as json_file:
+    data = json.load(json_file)
+    for p in data['issues']:
+        tid = p['id']
+        pjid = p['project']['id']
+        if not pjid in pjIds.keys():
+            pjIds[pjid] = []
+        if not pjid in pjNames.keys():
+            pjNames[pjid] = p['project']['name']
+        if p['closed_on'] and showClosed == False:
+            continue
+        if projects and not pjid in projects:
+            continue
+        pjIds[pjid].append(tid)
+        globalIds.append(tid)
+        subjects[tid] = p['subject']
+        trackers[tid] = p['tracker']['name']
+        closeds[tid] = p['closed_on']
+        ratios[tid] = int(p['done_ratio'])
+        if showColor == True:
+            if closeds[tid]:
+                status[tid] = fg(p['status']['name'], 243)
+            else:
+                status[tid] = fg(p['status']['name'], 32)
+            if ratios[tid] == 0:
+                ratios[tid] = fg(str(ratios[tid]), 200)
+            elif ratios[tid] == 100:
+                ratios[tid] = fg(str(ratios[tid]), 48)
+            else:
+                ratios[tid] = fg(str(ratios[tid]), 228)
+        else:
+            status[tid] = p['status']['name']
+            ratios[tid] = str(ratios[tid])
+        updateds[tid] = p['updated_on']
+        pjs[tid] = pjid
+
+def take_updated_on(tid):
+    return updateds[tid]
+
+def show_project(pj):
+    print("PJ%d\t%s" % (pj, pjNames[pj]))
+    sorted_tids = sorted(pjIds[pj], key=take_updated_on)
+    for tid in sorted_tids:
+        show_ticket(tid, False)
+    print("")
+
+def show_ticket(tid, showPj):
+    if showPj:
+        print("%d\t%s\tPJ%d\t<%s|%s|%s>\t%s" % (tid, updateds[tid], pjs[tid], trackers[tid], status[tid], ratios[tid],  subjects[tid]))
+    else:
+        print("%d\t%s\t<%s|%s|%s>\t%s" % (tid, updateds[tid], trackers[tid], status[tid], ratios[tid],  subjects[tid]))
+
+if not grouping:
+    sorted_tids = sorted(globalIds, key=take_updated_on)
+    for tid in sorted_tids:
+        show_ticket(tid, True)
+elif projects:
+    for pj in projects:
+        show_project(pj)
+else:
+    for pj in pjIds.keys():
+        show_project(pj)
