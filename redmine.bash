ed1202b8ecd652edceca79c63944407479b722a4
Naoya Horiguchi <nao.horiguchi@gmail.com>
2021-01-22 23:49:32 +0900

    remove RM_CHANGE_DIRECTORY
    Signed-off-by: Naoya Horiguchi <nao.horiguchi@gmail.com>

--- redmine.bash
+++ redmine.bash
@@ -2,72 +2,23 @@
 
 # this script should be sourced from .bashrc.
 
-if [ "$RM_CHANGE_DIRECTORY" == true ] ; then
-	redmine() {
-		local subcmd="$1"
-		local id="$2"
-		local thisdir=$(readlink -f $(dirname $BASH_SOURCE))
-
-		# TODO: doesn't work if global option is given.
-		if [ "$subcmd" == edit ] ; then
-			local issueid=${!#}
-
-			local subject="$(jq -r ".issues[] | select(.id == $issueid) | .subject" $RM_CONFIG/issues.json 2> /dev/null)"
-			local project="$(jq -r ".issues[] | select(.id == $issueid) | .project.name" $RM_CONFIG/issues.json 2> /dev/null)"
-
-			if [ "$issueid" ] && [ "$issueid" -ge 0 ] 2> /dev/null ; then
-				mkdir -p "$RM_CONFIG/edit_memo/$issueid"
-				if [ "$STY" ] ; then
-					printf $'\033k'RM#$issueid\($project\)$'\033'\\
-				else
-					echo -en "\033]2; RM#$issueid ($project): $subject\a"
-				fi
-				cd $RM_CONFIG/edit_memo/$issueid
-				bash $thisdir/scripts/main.sh $@
-			else
-				bash $thisdir/scripts/main.sh $subcmd -h
-				return
-			fi
-		elif [ "$subcmd" == open ] ; then
-			if [ ! "$BROWSER" ] ; then
-				echo "you have to set environment variable BROWSER"
-				return
-			fi
-			# In some environment (like WSL), opening browser might fail
-			# if it's called from subprocess.
-			if [ "$id" -gt 0 ] ; then
-				echo "open $RM_BASEURL/issues/$id with browser."
-				eval "$BROWSER $RM_BASEURL/issues/$id"
-			fi
-		elif [ "$subcmd" == dir ] ; then
-			local issueid=$2
-			if [ -e "$RM_CONFIG/edit_memo/$issueid" ] ; then
-				cd $RM_CONFIG/edit_memo/$issueid
-			fi
-		else
-			# TODO: what if argument include "multi word string"?
-			bash $thisdir/scripts/main.sh $@
+if [ "$STY" ] ; then
+	_catch_child_termination() {
+		if ! pgrep -P $$ > /dev/null ; then
+			[ "$STY" ] && PROMPT_COMMAND="$KEEP_PROMPT_COMMAND"
 		fi
-	}
-else
-	if [ "$STY" ] ; then
-		_catch_child_termination() {
-			if ! pgrep -P $$ > /dev/null ; then
-				[ "$STY" ] && PROMPT_COMMAND="$KEEP_PROMPT_COMMAND"
-			fi
-		}
-	fi
-
-	redmine() {
-		local thisdir=$(readlink -f $(dirname $BASH_SOURCE))
-
-		if [ "$STY" ] ; then
-			KEEP_PROMPT_COMMAND="$PROMPT_COMMAND"
-			PROMPT_COMMAND='_catch_child_termination'
-			local tag="$(echo $@ | sed 's/ /_/g')"
-			printf $'\033k'RM_$tag$'\033'\\
-		fi
-
-		bash $thisdir/scripts/main.sh $@
 	}
 fi
+
+redmine() {
+	local thisdir=$(readlink -f $(dirname $BASH_SOURCE))
+
+	if [ "$STY" ] ; then
+		KEEP_PROMPT_COMMAND="$PROMPT_COMMAND"
+		PROMPT_COMMAND='_catch_child_termination'
+		local tag="$(echo $@ | sed 's/ /_/g')"
+		printf $'\033k'RM_$tag$'\033'\\
+	fi
+
+	bash $thisdir/scripts/main.sh $@
+}
