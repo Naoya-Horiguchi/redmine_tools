3ecdfeb23c7556109e58688c05b3db2240be4317
Naoya Horiguchi <nao.horiguchi@gmail.com>
2021-01-22 23:46:06 +0900

    change screen window title based on redmine command
    Signed-off-by: Naoya Horiguchi <nao.horiguchi@gmail.com>

--- redmine.bash
+++ redmine.bash
@@ -50,5 +50,24 @@ if [ "$RM_CHANGE_DIRECTORY" == true ] ; then
 		fi
 	}
 else
-	alias redmine=$(readlink -f $(dirname $BASH_SOURCE))/scripts/main.sh
+	if [ "$STY" ] ; then
+		_catch_child_termination() {
+			if ! pgrep -P $$ > /dev/null ; then
+				[ "$STY" ] && PROMPT_COMMAND="$KEEP_PROMPT_COMMAND"
+			fi
+		}
+	fi
+
+	redmine() {
+		local thisdir=$(readlink -f $(dirname $BASH_SOURCE))
+
+		if [ "$STY" ] ; then
+			KEEP_PROMPT_COMMAND="$PROMPT_COMMAND"
+			PROMPT_COMMAND='_catch_child_termination'
+			local tag="$(echo $@ | sed 's/ /_/g')"
+			printf $'\033k'RM_$tag$'\033'\\
+		fi
+
+		bash $thisdir/scripts/main.sh $@
+	}
 fi
